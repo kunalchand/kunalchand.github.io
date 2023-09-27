9237a6f05efef8da67b9f09faeb5da1fd7347abe
Kunal Chand <kunalchand234@gmail.com>
2023-09-27 13:04:19 -0400

    Home & Nav Fix

--- portfolio2/script.js
+++ portfolio2/script.js
@@ -1,6 +1,31 @@
-function toggleMenu() {
-  const menu = document.querySelector(".menu-links");
-  const icon = document.querySelector(".hamburger-icon");
-  menu.classList.toggle("open");
-  icon.classList.toggle("open");
+let menuOpen = false;
+
+function toggleMenu(event) {
+  if (!menuOpen) {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.add("open");
+    icon.classList.add("open");
+    menuOpen = true;
+  } else {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.remove("open");
+    icon.classList.remove("open");
+    menuOpen = false;
+  }
+
+  event.stopPropagation();
+}
+
+// Close menu if clicking outside
+document.addEventListener("click", function (event) {
+  if (menuOpen) {
+    toggleMenu();
+  }
+});
+
+function showUrl(img, url_link) {
+  img.dataset.url = url_link;
+  img.title = img.dataset.url;
 }
