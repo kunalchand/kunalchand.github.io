dcfec0a47caea8e4de41dccea4195b68911d0db7
Kunal Chand <kunalchand234@gmail.com>
2023-09-27 20:41:30 -0400

    info bar content

--- portfolio2/script.js
+++ portfolio2/script.js
@@ -18,11 +18,45 @@ function toggleMenu(event) {
   event.stopPropagation();
 }
 
-// Close menu if clicking outside
+// Close menu & info bar if clicking outside
 document.addEventListener("click", function (event) {
   if (menuOpen) {
     toggleMenu();
   }
+  infoBar.style.display = "none";
+});
+
+const emailIcon = document.getElementById("email-icon");
+const phoneIcon = document.getElementById("phone-icon");
+const locationIcon = document.getElementById("location-icon");
+
+const infoBar = document.getElementById("info-bar");
+const infoText = document.getElementById("info-text");
+const copyButton = document.getElementById("copy-button");
+
+emailIcon.addEventListener("click", function () {
+  infoBar.style.display = "flex";
+  infoText.textContent = "kunalchand234@gmail.com";
+  copyButton.style.display = "flex";
+  event.stopPropagation();
+});
+
+phoneIcon.addEventListener("click", function () {
+  infoBar.style.display = "flex";
+  infoText.textContent = "+1 (716) 292-5504";
+  copyButton.style.display = "none";
+  event.stopPropagation();
+});
+
+locationIcon.addEventListener("click", function () {
+  infoBar.style.display = "flex";
+  infoText.textContent = "Buffalo, New York, USA";
+  copyButton.style.display = "none";
+  event.stopPropagation();
+});
+
+infoBar.addEventListener("click", function () {
+  event.stopPropagation();
 });
 
 function showUrl(img, url_link) {
